ee7185bf3b141c210fb23d1346525e4a68e593ac
Aleks23-0-24 <sanekkazmin2018@gmail.com>
2025-12-15 02:44:01 +0500

    added everything

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
-export const API_URL = process.env.API_URL || "http://192.168.228.33:3000/api";
+export const API_URL = process.env.API_URL || "http://172.20.10.13:3000/api";
 
 export default API_URL;
